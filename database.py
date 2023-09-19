e5f7a98bda8e358d771291b4e235f26fb3e2a12d
Fay Shaw <fayette@gmail.com>
2023-09-19 14:28:41 -0400

    Initial commit, working plot and database pull

--- database.py
+++ database.py
@@ -4,11 +4,8 @@ import os
 import argparse
 
 def get_wells(depth, gradient):
-    
-    connection = os.getenv("WELL_DB","")      
-    
+    connection = os.getenv("WELL_DB", "")
     engine = sqlalchemy.create_engine(connection)
-
     conn = engine.connect()
 
     query = """SELECT latitude, longitude, depth, gradient
@@ -16,13 +13,14 @@ def get_wells(depth, gradient):
     WHERE depth > :depth AND gradient > :grad;"""
 
     q = sqlalchemy.text(query)
+    
     parameters = {'depth': depth, 'grad': gradient}
 
     return conn.execute(q, parameters).fetchall()
         
-if __name__ == '__main__':  # if run from command line
+if __name__ == '__main__':
     parser = argparse.ArgumentParser("A utility to fetch data from a database of wells",
-                                     epilog = "You will need the WELL_DB env var set to a sqlalchemy connect string")
+                                     epilog = "You need to have the WELL_DB env var set to a sqlalchemy connect string")
     parser.add_argument("depth", type=float, help="Minimum depth of a well to output (in meters)")
     parser.add_argument("gradient", type=float, help="Minimum thermal gradient of a well to output (in deg C/m)")
     parser.add_argument("--name", type=str, help="Enter your name for a greeting")
--- well_plot.py
+++ well_plot.py
@@ -13,13 +13,14 @@ def make_chart(well_df):
         width=500,
         height=300
     ).project('albersUsa')
-
+    
+        
     well_chart = alt.Chart(well_df).mark_circle() \
                     .encode(latitude='latitude:Q', longitude='longitude:Q', \
-                            color=alt.Color('gradient', scale=alt.Scale(scheme='yelloworangered')), \
-                            tooltip=[alt.Tooltip('depth', title='Depth(m)'), 
-                                     alt.Tooltip('gradient', title='Gradient (°C/m)', format='0.3f')]
-                           )\
+                            color=alt.Color('gradient:N', scale=alt.Scale(scheme='yelloworangered')), \
+                            tooltip=[alt.Tooltip('depth:N', title='Depth(m)'), 
+                                      alt.Tooltip('gradient:N', title='Gradient (°C/m)', format='0.3f')]
+                            )\
                     .properties(title='Abandoned wells')
 
     return background + well_chart
